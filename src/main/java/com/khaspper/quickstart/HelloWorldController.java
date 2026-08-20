dc9642e0feb9ce4e8d13a999d7dab4c62654d29b
Markus <brandonnarciso24@gmail.com>
2026-08-19 21:36:51 -0700

    My first get request

--- src/main/java/com/khaspper/quickstart/HelloWorldController.java
+++ src/main/java/com/khaspper/quickstart/HelloWorldController.java
@@ -1,6 +1,16 @@
 package com.khaspper.quickstart;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Rest
+// This is so spring knows that hey this class it handles HTTP requests bro and returns
+// the response as a Json or XML
+@RestController
 public class HelloWorldController {
+
+    // This says YO BRO!!!!! at path "/hello" we return "Hello Markus!"
+    @GetMapping(path = "/hello")
+    public String helloWorld() {
+        return "Hello Markus!";
+    }
 }
--- src/main/java/com/khaspper/quickstart/QuickstartApplication.java
+++ src/main/java/com/khaspper/quickstart/QuickstartApplication.java
@@ -3,6 +3,7 @@ package com.khaspper.quickstart;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// An entry point for our spring boot application
 @SpringBootApplication
 public class QuickstartApplication {
 
